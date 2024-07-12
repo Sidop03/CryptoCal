daa446958d33a9b8fbe62f785c423dce252a65e8
Sidop03 <143225060+Sidop03@users.noreply.github.com>
2024-07-13 04:03:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
 import Convertor from './Convertor';
 import {Route,Routes} from "react-router-dom";
-import Navigation from './Navigation';
-
 
 function App() {
 	return (
 		<>
-			<Navigation/>
 				<Routes>
-					<Route path="/" element={<Convertor/>}/>
 					<Route path="/convertor" element={<Convertor/>}/>
+					<Route path="/" element={<Convertor/>}/>
+					<Route path="*" element={<Convertor/>}/>
 				</Routes>
+			
 		</>
 	);
 }
