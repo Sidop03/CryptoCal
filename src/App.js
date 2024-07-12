61038a9878bb7f4f00077c0291f577503729073c
Sidop03 <143225060+Sidop03@users.noreply.github.com>
2024-07-13 03:53:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import './App.css';
 import Convertor from './Convertor';
-import NotFound from './pages/NotFound';
-import About from './pages/About';
 import {Route,Routes} from "react-router-dom";
 import Navigation from './Navigation';
-import Profile from './pages/Profile';
+
 
 function App() {
 	return (
 		<>
 			<Navigation/>
 				<Routes>
+					<Route path="/" element={<Convertor/>}/>
 					<Route path="/convertor" element={<Convertor/>}/>
-					<Route path="/about" element={<About/>}/>
-					<Route path="*" element={<NotFound/>}/>
-					<Route path='/user/:UserName/:project' element={<Profile/>}/>
 				</Routes>
-			
 		</>
 	);
 }
